724dc3ab9db1fba06ca75d6c6a86e4db27b9279f
mooinglemur <mooinglemur@users.noreply.github.com>
2026-03-11 12:51:14 -0700

    switch compilers for cross build

--- Toolchain-mingw.cmake
+++ Toolchain-mingw.cmake
@@ -2,8 +2,8 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 # Specify the cross compiler
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
 # Target environment prefix
